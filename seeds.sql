2a178522b0f551257453b13591d7efa9e1ea47e0
vsteere <vssteere@gmail.com>
2020-09-24 13:10:28 -0500

    updated seeds file content to correct parameters

--- seeds.sql
+++ seeds.sql
@@ -1,8 +1,9 @@
 INSERT INTO department (name)
-VALUES ('John Smith'), ('Jane Smith'), ('Ivan Ivanov'), ('Sergey Ilyenkov'), ('Jane Doe'), ('John Doe');
+VALUES ('Production'), ('Design'), ('Administration'), ('Logistics');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Engineer', 95000, 1), ('Senior Engineer', 125000, 2), ('Manager', 150000, 3), ('Manager', 150000, 4), ('DBA', 105000, 5), ('Analyst', 100000, 6);
+VALUES ('Engineer', 95000, 1), ('Senior Engineer', 125000, 2), ('Manager', 150000, 1), ('Manager', 150000, 2), ('DBA', 105000, 3), ('Analyst', 100000, 4);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Smith', 1, 3), ('Jane', 'Smith', 2, 3), ('Ivan', 'Ivanov', 3, NULL), ('Sergey', 'Ilyenkov', 4, NULL), ('Jane', 'Doe', 5, 4), ('John', 'Doe', 6, 4);
