8b1a65fec9ec8ef96950bfb5d4484dd6a735985e
vsteere <vssteere@gmail.com>
2020-09-24 18:49:42 -0500

    added inquirer and switch statement for main menu

--- server.js
+++ server.js
@@ -15,7 +15,60 @@ var connection = mysql.createConnection({
   database: "top_songsDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  runSearch();
+  mainMenu();
 });
+//function for the main menu 
+function mainMenu() {
+
+
+
+  inquirer.prompt([
+    { type: "list", name: "mainChoices", message: "Please Select Action", choices: ["Add a new Department", "Add a new Role", "Add a new Employee", "View a listing of Departments", "Update Employee Roles", "View Staff", "Exit"] }
+
+  ])
+
+
+    .then(response => {
+
+      //switch statement that will call a function depending on which option a user selects
+      switch (response.mainChoices) {
+        case "Add a new Department":
+          newDept()
+          break
+
+        case "Add a New Role":
+          newRole()
+          break
+
+        case "View a listing of Departments":
+          deptList()
+          break
+
+        case "Update Employee Roles":
+          updateRoles()
+          break
+
+        case "View Staff":
+          viewStaff()
+          break
+
+        case "Add a new Employee":
+          newEmployee()
+          break
+
+        //this is the ending option that will quit the application
+        default: exit()
+
+      }
+
+    })
+
+
+}
+
+
+
+
+} 
